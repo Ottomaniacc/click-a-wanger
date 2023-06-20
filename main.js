6d39bdd9251996eef9ceb79ba027b5861b0842b4
Ottomaniacc <302899406@student.rocmondriaan.nl>
2023-06-21 01:14:18 +0200

    swag

---
+++ main.js
@@ -0,0 +1,21 @@
+
+let nameInHtml = document.querySelector('.username');
+let score = document.querySelector('.score');
+let button = document.querySelector('.button');
+
+function showInConsole(){
+    console.log(button)
+}
+button.addEventListener("click", showInConsole())
+
+
+
+// let userNameinp = prompt('what is your name nigga');
+
+
+
+// let userName = userNameinp;
+
+// nameInHtml.textContent +=  userNameinp + " !"; 
+
+
