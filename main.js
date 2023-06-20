5a188f821c6f0912591241827b35eca83091ef21
Ottomaniacc <302899406@student.rocmondriaan.nl>
2023-06-21 01:48:58 +0200

    wagner game finished

--- main.js
+++ main.js
@@ -2,10 +2,12 @@
 let nameInHtml = document.querySelector('.username');
 let score = document.querySelector('.score');
 let button = document.querySelector('.button');
+let rank = document.querySelector('.level');
 
 let scoreValue = {
     score: 0,
 }
+
 
 
 let userNameinp = prompt('what is your name nigga');
@@ -19,7 +21,26 @@ function myFunction(){
     
     score.innerHTML = scoreValue.score;
     
+
+    if(scoreValue.score == 10){
+        rank.innerHTML = "timmy"
+    } else if (scoreValue.score == 30){
+        rank.innerHTML = "drunkato"
+    } else if (scoreValue.score == 50){
+        rank.innerHTML = "jelqer"
+    } else if (scoreValue.score == 70){
+        rank.innerHTML = "reshalla"
+    } else if (scoreValue.score == 80){
+        rank.innerHTML = "retard nigga penis"
+    } else if (scoreValue.score == 101){
+        rank.innerHTML = "wagner"
+        alert('omg you won wow')
+        location.reload()
+    }
+
+
 }
+
 
 
 button.addEventListener("click", myFunction);
